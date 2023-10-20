0715d3a40f5dd8f2b139019c3c41199b0b79d97b
datdo <datdo27120003@gmail.cco>
2023-10-20 11:21:33 +0700

    123

--- face-api.js/examples/examples-browser/model/user-data.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require('mongoose');
-
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-const userData=mongoose.Schema({image: Buffer}, { _id: false })
-
-userData.plugin(AutoIncrement);
-
-module.exports=mongoose.model('user-datas',userData)
